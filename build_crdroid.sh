a9c035f5296c29d599a596dfd5dae14df57948da
rsuntk <rsuntk@yukiprjkt.my.id>
2025-10-04 20:51:55 +0700

    [build]: add more script
    Signed-off-by: rsuntk <rsuntk@yukiprjkt.my.id>

---
+++ build_crdroid.sh
@@ -0,0 +1,35 @@
+# cleanup
+rm -rf .repo/local_manifests/ prebuilts/clang/host/linux-x86
+
+# init repo
+repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/rsuplaygrnd/crdroid_manifests.git -b 16.0 -g default,-mips,-darwin,-notdefault
+
+# clone local manifests
+git clone https://github.com/rsuplaygrnd/local_manifest --depth 1 -b X01BD-16.0 .repo/local_manifests
+
+# repo sync
+[ -f /usr/bin/resync ] && /usr/bin/resync || /opt/crave/resync.sh
+
+# cleanup #2
+[ -d out ] && rm -rf out/target || echo "[-] No need to clean out/"
+
+# setup KernelSU
+if [ -d kernel/asus/sdm660 ]; then 
+	cd kernel/asus/sdm660
+	curl -LSs "https://raw.githubusercontent.com/rsuntk/KernelSU/main/kernel/setup.sh" | bash -s main
+	cd ../../..
+fi
+
+# Set up build environment
+export BUILD_USERNAME=rsuntk 
+export BUILD_HOSTNAME=nobody 
+export TZ="Asia/Jakarta"
+source build/envsetup.sh
+
+# Build the ROM
+lunch lineage_X01BD-bp2a-userdebug
+# cleanup #3
+make installclean
+mka bacon
+
+[ -d out ] && ls out/target/product/X01BD
