3c2b8c2cf060741686dd7727b58fe3488e176524
fc5b87cf <rissu.ntk@gmail.com>
2025-11-16 14:15:02 +0700

    build: update command
    Signed-off-by: fc5b87cf <rissu.ntk@gmail.com>

--- build_mica.sh
+++ build_mica.sh
@@ -11,22 +11,24 @@ git clone https://github.com/rsuplaygrnd/local_manifest --depth 1 -b bp3 .repo/l
 [ -f /usr/bin/resync ] && /usr/bin/resync || /opt/crave/resync.sh
 
 # setup KernelSU
-if [ -d kernel/asus/sdm660 ]; then 
+if [ -d kernel/asus/sdm660 ]; then
 	cd kernel/asus/sdm660
 	curl -LSs "https://raw.githubusercontent.com/rsuntk/KernelSU/main/kernel/setup.sh" | bash -s main
 	cd ../../..
 fi
 
 # Set up build environment
-export BUILD_USERNAME=rsuntk 
-export BUILD_HOSTNAME=nobody 
+export BUILD_USERNAME=rsuntk
+export BUILD_HOSTNAME=nobody
 export TZ="Asia/Jakarta"
 source build/envsetup.sh
+
+export TARGET_EXCLUDE_GMS=true
 
 # Build the ROM
 lunch mica_X01BD-bp3a-userdebug
 # cleanup #3
 make installclean
-m
+m mica-release
 
 [ -d out ] && ls out/target/product/X01BD
