80cef5be2afa32213cf3eb84647a1c253688d85b
fc5b87cf <rissu.ntk@gmail.com>
2025-11-24 18:45:27 +0700

    clover
    Signed-off-by: fc5b87cf <rissu.ntk@gmail.com>

---
+++ cook_clover.sh
@@ -0,0 +1,45 @@
+# cleanup
+remove_lists=(
+.repo/local_manifests
+prebuilts/clang/host/linux-x86
+device/qcom/sepolicy
+device/qcom/sepolicy-legacy-um
+device/qcom/sepolicy_vndr/legacy-um
+device/asus/sdm660-common
+device/asus/X01BD
+kernel/asus/sdm660
+out/target/product/X01BD
+)
+
+rm -rf "${remove_lists[@]}"
+
+# init repo
+repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/rsuplaygrnd/clover.git -b 16-qpr1 -g default,-mips,-darwin,-notdefault
+
+# clone local manifests
+git clone https://github.com/rsuplaygrnd/local_manifest.git --depth 1 -b clover-bp3a .repo/local_manifests
+
+# repo sync
+[ -f /usr/bin/resync ] && /usr/bin/resync || /opt/crave/resync.sh
+
+# setup KernelSU
+if [ -d kernel/asus/sdm660 ]; then
+	cd kernel/asus/sdm660
+	curl -LSs "https://raw.githubusercontent.com/rsuntk/KernelSU/main/kernel/setup.sh" | bash -s main
+	cd ../../..
+fi
+
+# Set up build environment
+export BUILD_USERNAME=rsuntk
+export BUILD_HOSTNAME=nobody
+export TZ="Asia/Jakarta"
+source build/envsetup.sh
+
+export TARGET_EXCLUDE_GMS=true
+
+# Build the ROM
+lunch clover_X01BD-bp3a-userdebug
+make installclean
+m mica-release
+
+[ -d out ] && ls out/target/product/X01BD
